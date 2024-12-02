af048a1f91aa9689ba9e049071cc5592b6773df4
zhao <zhao12501@163.com>
2024-12-02 09:09:24 +0800

    增加 login

--- SweetFamilyAffairs/urls.py
+++ SweetFamilyAffairs/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include, re_path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.views import WxUserViewSet
+from users.views import WxUserLoginAPIView, WxUserViewSet
 
 router = DefaultRouter()
 router.register(r'users', WxUserViewSet)
 urlpatterns = [
     path('', include('items.urls')),
     path('', include(router.urls)),
+    path('login/', WxUserLoginAPIView.as_view(), name='login'),
 ]
